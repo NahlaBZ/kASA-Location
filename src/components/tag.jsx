c525f4c84088885b5c682828484c3e5adda33272
NahlaBZ <benzidnahla@gmail.com>
2023-06-15 16:26:44 +0200

    Modification de la page de logement

---
+++ src/components/tag.jsx
@@ -0,0 +1,13 @@
+
+
+function Tag(props) {
+
+
+    return (
+        <>
+            <p className="tag" >{props.tag}</p>
+        </>
+    )
+}
+
+export default Tag
--- src/pages/Location.jsx
+++ src/pages/Location.jsx
@@ -1,6 +1,8 @@
 import { useParams } from "react-router-dom";
 import Carroussel from "../components/Loc/Carrousel";
-import logement from "../Data/logement.json"
+import logement from "../Data/logement.json";
+import Collapse from "../components/Collapse";
+import Tag from "../components/tag";
 
 
 function Location() {
@@ -17,12 +19,42 @@ function Location() {
     )
   })
 
-
+  let tags = checkedRental.tags.map((tag, index) => (
+    <Tag key={`${index}`} tag={`${tag}`} />
+  ))
 
   return (
     <>
       <Carroussel image={pictures} />
 
+      <section className="rentalIdentity" >
+        <div className="place" >
+          <p className="rentalTitle" >{checkedRental.title}</p>
+          <p className="rentalPlace" >{checkedRental.location}</p>
+          <div className="tagWrapper">
+            {tags}
+          </div>
+        </div>
+        <div className="hostAndRate" >
+          <div className="host" >
+            <p className="renterName" >{checkedRental.host.name}</p>
+            <img className="renterPicture" src={checkedRental.host.picture} alt="Profil du loueur" />
+          </div>
+
+        </div>
+      </section>
+
+      <div className="collapse-container">
+        <Collapse title="Description">{checkedRental.description}</Collapse>
+        <Collapse title="Équipements">
+          {checkedRental.equipments.map((equipment) => (
+            <p key={equipment}>
+              {equipment}
+              <br />
+            </p>
+          ))}
+        </Collapse>
+      </div>
 
 
     </>
